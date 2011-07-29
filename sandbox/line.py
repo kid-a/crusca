d22fe605dc3adacdf8475c42940d0ade52f6bc65
kid-a <loris.fichera@gmai.com>
2011-07-29 15:21:32 +0200

    playing with matplotlib and events

---
+++ sandbox/line.py
@@ -0,0 +1,32 @@
+from matplotlib.widgets import Cursor
+import pylab
+
+fig = pylab.figure(figsize=(8,6))
+ax = fig.add_axes([0.075, 0.25, 0.9, 0.725], axisbg='#FFFFCC')
+
+x,y = 4*(pylab.rand(2,100)-.5)
+ax.plot(x,y,'o')
+ax.set_xlim(-2,2)
+ax.set_ylim(-2,2)
+
+def onclick(event):
+    print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(
+        event.button, event.x, event.y, event.xdata, event.ydata)
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+# set useblit = True on gtkagg for enhanced performance    
+cursor = Cursor(ax, useblit=True, color='red', linewidth=2 )
+
+pylab.show()
+
+
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(np.random.rand(10))
+
+
+
+# cid = fig.canvas.mpl_connect('button_press_event', onclick)
