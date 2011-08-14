c9fc472db069a1cf717921185807a693c7077754
Loris Fichera <loris.fichera@gmail.com>
2011-08-14 16:23:23 +0200

    added clouds app

---
+++ pywordcloud/clouds/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Cloud(models.Model):
+    cloud_id = models.BigIntegerField (primary_key = True)
+    name = models.CharField (max_length = 200)
+    text = models.TextField (blank = True)
+    datetime_created = models.DateTimeField ('date of creation')
+    datetime_updated = models.DateTimeField ('last updated')
+    image = models.ImageField (max_length = 500, blank = True)
+    ip_address = models.IPAddressField (blank = True)
